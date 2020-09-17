e563f09b1fd30ca4dd259a8960ae0a35632cbd94
Patti Scalfano <pjs@foundationsoft.com>
2020-09-17 10:35:29 -0400

    Test Github

--- PJSAzure.API/Controllers/WeatherForecastController.cs
+++ PJSAzure.API/Controllers/WeatherForecastController.cs
@@ -35,5 +35,19 @@ namespace PJSAzure.API.Controllers
             })
             .ToArray();
         }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IEnumerable<WeatherForecast> GetById(int id)
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
     }
 }
